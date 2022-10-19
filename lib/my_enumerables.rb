af52a09d66b0b576dcab8498a808bcf2540fa3e3
swifteye1111 <hasak.josh@gmail.com>
2022-10-19 10:36:30 +0100

    First commit. Create #my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -2,10 +2,28 @@ module Enumerable
   # Your code goes here
 end
 
+# Enumerable#my_each_with_index	spec/my_each_with_index_spec.rb
+# Enumerable#my_select	spec/my_select_spec.rb
+# Enumerable#my_all?	spec/my_all_spec.rb
+# Enumerable#my_any?	spec/my_any_spec.rb
+# Enumerable#my_none?	spec/my_none_spec.rb
+# Enumerable#my_count	spec/my_count_spec.rb
+# Enumerable#my_map	spec/my_map_spec.rb
+# Enumerable#my_inject	spec/my_inject_spec.rb
+
 # You will first have to define my_each
 # on the Array class. Methods defined in
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    return unless block_given?
+
+    i = 0
+    while i < self.length
+      yield self[i]
+      i += 1
+    end
+    self
+  end
 end
